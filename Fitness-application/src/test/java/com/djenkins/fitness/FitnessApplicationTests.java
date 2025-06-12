f0592a318ddcc1bd3fb6a5b936b2746a0281470b
ice <itsmeusnice@gmail.com>
2025-06-12 23:26:08 +0200

    Initial Commit

---
+++ Fitness-application/src/test/java/com/djenkins/fitness/FitnessApplicationTests.java
@@ -0,0 +1,12 @@
+package com.djenkins.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
